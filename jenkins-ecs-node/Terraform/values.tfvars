e9a067e91c51a6f83ab85a6aaadef6ced43037f2
Mayank Gandhe <mayankgandhe@Mayanks-MacBook-Air.local>
2021-10-04 10:15:35 +0530

    Added files to git ignore

---
+++ jenkins-ecs-node/Terraform/values.tfvars
@@ -0,0 +1,5 @@
+ecsClusterName = "test"
+controler_sg =  "sg-081ae9b30695fa78d"
+region = "us-east-2"
+vpcId =  "vpc-088eb6d3710e20103"
+tags = {"Env" = "Testing"}
---
+++ jenkins-ecs-node/Terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "controler_sg" {
+  description = "Security Group of the jenkins master to whitelist in the SG of ECS cluster"
+}
+variable "ecsClusterName" {
+  description = "Name of cluster"
+}
+variable "vpcId" {
+  description = "Id of the VPC for SG creation"
+}
+variable "spotEnabled" {
+  type    = bool
+  default = true
+}
+variable "tags" {
+    type = map
+    description = "Default tags to be applied to every resource"
+  
+}
